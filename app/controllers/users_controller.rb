64a5f04b97c3f14907b771d880818f40c08418a9
YOUR NAME <YOUR@EMAIL.com>
2020-08-12 16:56:09 +0700

    dang fix chua xong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,18 +11,19 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 	def create
-		@user = User.new user_params
+		@user = User.new(user_params)
 		if @user.save
+			log_in @user
 			flash[:success] = "Welcome to the Sample App!"
 			redirect_to @user
 		# Handle a successful save.
 		else
 			render :new
 		end
+	end	
 	private
 	def user_params
 		params.require(:user).permit(:name, :email, :password,
 									 	:password_confirmation)
 	end
-end
 end
