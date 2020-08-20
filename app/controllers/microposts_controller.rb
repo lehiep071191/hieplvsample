9730f49f000b6d6849cc9cd96c7188ba462f2e33
lehiep071191 <good.boy.0991@gmail.com>
2020-08-20 20:46:14 +0700

    chapter 13

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,32 @@
+class MicropostsController < ApplicationController
+
+	before_action :logged_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: :destroy
+
+	def create
+	@micropost = current_user.microposts.build(micropost_params)
+	@micropost.image.attach(params[:micropost][:image])
+		if @micropost.save
+			flash[:success] = "Micropost created!"
+			redirect_to help_path
+		else
+			@feed_items = current_user.feed.paginate(page: params[:page])
+			render 'static_pages/help'
+		end
+	end
+	def destroy
+		@micropost.destroy
+		flash[:success] = "Micropost deleted"
+		redirect_to request.referrer || root_url
+	end
+
+	private
+
+	def micropost_params
+		params.require(:micropost).permit(:content, :image)
+	end
+	def correct_user
+		 @micropost = Micropost.find_by(id: params[:id])
+		redirect_to root_url if !current_user.current_user?(@micropost.user)
+	end
+end
